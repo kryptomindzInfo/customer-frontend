6110e2f0ca2f14fc1c9c73b6ca0cd9fc16458692
beyondewallet <58459451+beyondewallet@users.noreply.github.com>
2020-03-20 10:49:50 +0530

    Update constants.js

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -19,9 +19,15 @@ export const LOAD_REPOS_ERROR = 'boilerplate/App/LOAD_REPOS_ERROR';
 // export const CONTRACT_URL = 'http://localhost:3001/uploads/';
 
 //for server
-export const API_URL = 'http://localhost:3001/api';
-export const STATIC_URL = 'http://localhost:3001/uploads/';
-export const CONTRACT_URL = 'http://34.68.116.5:8080/ipfs/';
+// export const API_URL = 'http://localhost:3001/api';
+// export const STATIC_URL = 'http://localhost:3001/uploads/';
+// export const CONTRACT_URL = 'http://34.68.116.5:8080/ipfs/';
 
+// production config k8s
+export const SERVER_URL = '91d90ac373dc.sn.mynetname.net:2020';
+export const API_URL = 'http://91d90ac373dc.sn.mynetname.net:2020/api';
+export const STATIC_URL = 'http://91d90ac373dc.sn.mynetname.net:2020/uploads/';
+export const CONTRACT_URL = 'http://91d90ac373dc.sn.mynetname.net:2020/ipfs/';
+// end production config
 
 export const CURRENCY = 'XOF';
