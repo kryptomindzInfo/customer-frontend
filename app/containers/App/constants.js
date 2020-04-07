99242853aa58b07c4cf84b353ae3927e5ee7ea00
Yusuf <yjkheraluwala@example.com>
2020-04-07 11:31:14 +0530

    added server api url

--- app/containers/App/constants.js
+++ app/containers/App/constants.js
@@ -19,7 +19,7 @@ export const LOAD_REPOS_ERROR = 'boilerplate/App/LOAD_REPOS_ERROR';
 // export const CONTRACT_URL = 'http://localhost:3001/uploads/';
 
 // for server
-export const API_URL = 'http://localhost:3001/api';
-export const STATIC_URL = 'http://localhost:3001/uploads/';
-export const CONTRACT_URL = 'http://localhost:3001/uploads/';
+export const API_URL = 'http://91d90ac373dc.sn.mynetname.net:2020/api';
+export const STATIC_URL = 'http://91d90ac373dc.sn.mynetname.net:2020/uploads/';
+export const CONTRACT_URL = 'http://91d90ac373dc.sn.mynetname.net:2020/ipfs/';
 export const CURRENCY = 'XOF';
